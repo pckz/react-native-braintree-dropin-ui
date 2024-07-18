a2ad3482855f5b1ad37a007ea07a5eed2c29760a
FiSP <fispne@gmail.com>
2024-07-18 14:45:24 +0600

    Revert "debug code 1"
    This reverts commit d3e24486

--- android/src/main/java/tech/power/RNBraintreeDropIn/RNBraintreeDropInPackage.java
+++ android/src/main/java/tech/power/RNBraintreeDropIn/RNBraintreeDropInPackage.java
@@ -14,17 +14,15 @@ import android.content.Context;
 import expo.modules.core.interfaces.Package;
 import expo.modules.core.interfaces.ReactActivityLifecycleListener;
 
-public class RNBraintreeDropInActivityPackage implements Package {
-  @Override
-  public List<? extends ReactActivityLifecycleListener> createReactActivityLifecycleListeners(Context activityContext) {
-    print("RNBraintreeDropInActivityPackageRNBraintreeDropInActivityPackageRNBraintreeDropInActivityPackage");
-
-    return Collections.emptyList();
-          //return Collections.singletonList(new MyLibReactActivityLifecycleListener());
-  }
-}
-
 public class RNBraintreeDropInPackage implements ReactPackage {
+    @Override
+    public List<? extends ReactActivityLifecycleListener> createReactActivityLifecycleListeners(Context activityContext) {
+      print("RNBraintreeDropInActivityPackageRNBraintreeDropInActivityPackageRNBraintreeDropInActivityPackage");
+
+      return Collections.emptyList();
+          //return Collections.singletonList(new MyLibReactActivityLifecycleListener());
+    }
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNBraintreeDropInModule(reactContext));
